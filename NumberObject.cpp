fb53225e96ea2f3a2ff980b1914589876c6bb71f
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-04-29 15:15:13 +0800

    Update NumberObject.cpp

--- NumberObject.cpp
+++ NumberObject.cpp
@@ -8,26 +8,25 @@
 using namespace std;
 
 //NumberObject
-
-ostream& operator<<(ostream &io, NumberObject &num)
+ostream& operator<<(ostream& io, NumberObject& num)
 {
-	if(num.point_index == 0)
-	{
-		if(num.positive > 0)
-		{
-			string temp=num.number;
-			reverse(temp.begin(),temp.end());
-			io<<temp<<endl;
-			return io;
-		}
-		else
-		{
-			string temp=num.number;
-			reverse(temp.begin(),temp.end());
-			io<<"-"<<temp<<endl;
-			return io;
-		}
-	}
+    if (num.point_index == 0)
+    {
+        if (num.positive > 0)
+        {
+            string temp = num.number;
+            reverse(temp.begin(), temp.end());
+            io << temp << endl;
+            return io;
+        }
+        else
+        {
+            string temp = num.number;
+            reverse(temp.begin(), temp.end());
+            io << "-" << temp << endl;
+            return io;
+        }
+    }
     else
     {
         if (num.positive > 0)
@@ -47,10 +46,10 @@ ostream& operator<<(ostream &io, NumberObject &num)
         }
         else
         {
-            string temp=num.number;
-            int c=temp.length();
-            reverse(temp.begin(),temp.end());
-            io<<"-"<<temp<<endl;
+            string temp = num.number;
+            int c = temp.length();
+            reverse(temp.begin(), temp.end());
+            io << "-" << temp << endl;
             for (int i = 0; i < temp.length(); i++)
             {
                 if (temp.length() - i == 100)
@@ -62,7 +61,7 @@ ostream& operator<<(ostream &io, NumberObject &num)
             io << endl;
             return io;
         }
-	}
+    }
 }
 
 //int
@@ -132,28 +131,23 @@ Integer operator-(const Integer& n1, const Integer& n2)
     num1 = n1.number;
     num2 = n2.number;
 
-    //check if it's positive
+    //check if the answer is positive
     if (num1.length() > num2.length()){}
     else if (num2.length() > num1.length()) {
         swap(num1, num2);
         a.positive = -1;
     }
     else {
-        bool equal = true;
         for (int i = num1.length() - 1; i >= 0; i--) {
             if (num1[i] > num2[i]) {
-                equal = false;
                 break;
             }
-            if (num2[i] > num1[i]) {
+            if (num1[i] < num2[i]) {
                 swap(num1, num2);
                 a.positive = -1;
-                equal = false;
                 break;
             }
         }
-        if (equal)
-            cout << "equal" << endl;
     }
 
     //substraction
@@ -179,11 +173,11 @@ Integer operator-(const Integer& n1, const Integer& n2)
         }
     }
 
-    for (int i = ans.size() - 1; i >= 0; i--) {
+    //clear redundant zeros ex.1000 - 999 = 0001 -> 1
+    for (int i = ans.size() - 1; i > 0; i--) {
         if (ans[i] != 0)
             break;
-        if (ans.back() == 0)
-            ans.pop_back();
+        ans.pop_back();
     }
 
     string temp;
@@ -233,4 +227,353 @@ Integer operator*(const Integer &num1, const Integer &num2)
     }
     re.positive = num1.positive * num2.positive;
     return re;
+}
+
+Decimal::Decimal() 
+{ 
+    point_index = 1; positive = 1;
+}
+
+Decimal::Decimal(const Decimal& reference)
+{
+    number = reference.number;
+    point_index = reference.point_index;
+    positive = reference.positive;
+}
+
+Decimal operator+(const Decimal& n1, const Integer& n2)
+{
+    string num1 = n1.number, num2 = n2.number;
+    num2.insert(0, 100, '0');
+    vector<int>ans;
+
+    int num1_len = num1.length(), num2_len = num2.length();
+    if (num1_len > num2_len) {
+        swap(num1, num2);
+        swap(num1_len, num2_len);
+    }
+
+    int carry = 0;
+    for (int i = 0; i < num2_len; i++) {
+        if (i < num1_len) {
+            int t = (num1[i] - '0') + (num2[i] - '0') + carry;
+            if (t >= 10) {
+                carry = 1;
+                t -= 10;
+            }
+            else carry = 0;
+            ans.push_back(t);
+        }
+        else {
+            int t = (num2[i] - '0') + carry;
+            if (t >= 10) {
+                carry = 1;
+                t -= 10;
+            }
+            else carry = 0;
+            ans.push_back(t);
+        }
+    }
+    if (carry == 1) ans.push_back(1);
+
+    string temp;
+    for (int i = 0; i < ans.size(); i++) {
+        temp.append(1, (char)(ans[i] + '0'));
+    }
+
+    Decimal a;
+    a.number = temp;
+    a.positive = 1;
+    return a;
+}
+
+Decimal operator-(const Decimal& n1, const Integer& n2)
+{
+    Decimal a;
+    string num1, num2;
+    vector<int> ans;
+    num1 = n1.number;
+    num2 = n2.number;
+    num2.insert(0, 100, '0');
+
+    //check if the answer is positive
+    if (num1.length() > num2.length()) {}
+    else if (num2.length() > num1.length()) {
+        swap(num1, num2);
+        a.positive = -1;
+    }
+    else {
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            if (num1[i] > num2[i]) {
+                break;
+            }
+            if (num1[i] < num2[i]) {
+                swap(num1, num2);
+                a.positive = -1;
+                break;
+            }
+        }
+    }
+
+    //substraction
+    int borrow = 0, num1_len = num1.length(), num2_len = num2.length();
+    for (int i = 0; i < num1_len; i++) {
+        if (i < num2_len) {
+            int t = (num1[i] - '0') - (num2[i] - '0') + borrow;
+            if (t < 0) {
+                borrow = -1;
+                t += 10;
+            }
+            else borrow = 0;
+            ans.push_back(t);
+        }
+        else {
+            int t = (num1[i] - '0') + borrow;
+            if (t < 0) {
+                borrow = -1;
+                t += 10;
+            }
+            else borrow = 0;
+            ans.push_back(t);
+        }
+    }
+
+    //clear redundant zeros ex.1000 - 999 = 0001 -> 1
+    for (int i = ans.size() - 1; i > 0; i--) {
+        if (ans[i] != 0)
+            break;
+        ans.pop_back();
+    }
+
+    string temp;
+    for (int i = 0; i < ans.size(); i++) {
+        temp.append(1, (char)(ans[i] + '0'));
+    }
+
+    a.number = temp;
+    return a;
+}
+
+Decimal operator+(const Integer& n1, const Decimal& n2)
+{
+    string num1 = n1.number, num2 = n2.number;
+    num1.insert(0, 100, '0');
+    vector<int>ans;
+
+    int num1_len = num1.length(), num2_len = num2.length();
+    if (num1_len > num2_len) {
+        swap(num1, num2);
+        swap(num1_len, num2_len);
+    }
+
+    int carry = 0;
+    for (int i = 0; i < num2_len; i++) {
+        if (i < num1_len) {
+            int t = (num1[i] - '0') + (num2[i] - '0') + carry;
+            if (t >= 10) {
+                carry = 1;
+                t -= 10;
+            }
+            else carry = 0;
+            ans.push_back(t);
+        }
+        else {
+            int t = (num2[i] - '0') + carry;
+            if (t >= 10) {
+                carry = 1;
+                t -= 10;
+            }
+            else carry = 0;
+            ans.push_back(t);
+        }
+    }
+    if (carry == 1) ans.push_back(1);
+
+    string temp;
+    for (int i = 0; i < ans.size(); i++) {
+        temp.append(1, (char)(ans[i] + '0'));
+    }
+
+    Decimal a;
+    a.number = temp;
+    a.positive = 1;
+    return a;
+}
+
+Decimal operator-(const Integer& n1, const Decimal& n2)
+{
+    Decimal a;
+    string num1, num2;
+    vector<int> ans;
+    num1 = n1.number;
+    num2 = n2.number;
+    num1.insert(0, 100, '0');
+
+    //check if the answer is positive
+    if (num1.length() > num2.length()) {}
+    else if (num2.length() > num1.length()) {
+        swap(num1, num2);
+        a.positive = -1;
+    }
+    else {
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            if (num1[i] > num2[i]) {
+                break;
+            }
+            if (num1[i] < num2[i]) {
+                swap(num1, num2);
+                a.positive = -1;
+                break;
+            }
+        }
+    }
+
+    //substraction
+    int borrow = 0, num1_len = num1.length(), num2_len = num2.length();
+    for (int i = 0; i < num1_len; i++) {
+        if (i < num2_len) {
+            int t = (num1[i] - '0') - (num2[i] - '0') + borrow;
+            if (t < 0) {
+                borrow = -1;
+                t += 10;
+            }
+            else borrow = 0;
+            ans.push_back(t);
+        }
+        else {
+            int t = (num1[i] - '0') + borrow;
+            if (t < 0) {
+                borrow = -1;
+                t += 10;
+            }
+            else borrow = 0;
+            ans.push_back(t);
+        }
+    }
+
+    //clear redundant zeros ex.1000 - 999 = 0001 -> 1
+    for (int i = ans.size() - 1; i > 0; i--) {
+        if (ans[i] != 0)
+            break;
+        ans.pop_back();
+    }
+
+    string temp;
+    for (int i = 0; i < ans.size(); i++) {
+        temp.append(1, (char)(ans[i] + '0'));
+    }
+
+    a.number = temp;
+    return a;
+}
+
+Decimal operator+(const Decimal& n1, const Decimal& n2)
+{
+    string num1 = n1.number, num2 = n2.number;
+    vector<int>ans;
+
+    int num1_len = num1.length(), num2_len = num2.length();
+    if (num1_len > num2_len) {
+        swap(num1, num2);
+        swap(num1_len, num2_len);
+    }
+
+    int carry = 0;
+    for (int i = 0; i < num2_len; i++) {
+        if (i < num1_len) {
+            int t = (num1[i] - '0') + (num2[i] - '0') + carry;
+            if (t >= 10) {
+                carry = 1;
+                t -= 10;
+            }
+            else carry = 0;
+            ans.push_back(t);
+        }
+        else {
+            int t = (num2[i] - '0') + carry;
+            if (t >= 10) {
+                carry = 1;
+                t -= 10;
+            }
+            else carry = 0;
+            ans.push_back(t);
+        }
+    }
+    if (carry == 1) ans.push_back(1);
+
+    string temp;
+    for (int i = 0; i < ans.size(); i++) {
+        temp.append(1, (char)(ans[i] + '0'));
+    }
+
+    Decimal a;
+    a.number = temp;
+    a.positive = 1;
+    return a;
+}
+
+Decimal operator-(const Decimal& n1, const Decimal& n2)
+{
+    Decimal a;
+    string num1, num2;
+    vector<int> ans;
+    num1 = n1.number;
+    num2 = n2.number;
+
+    //check if the answer is positive
+    if (num1.length() > num2.length()) {}
+    else if (num2.length() > num1.length()) {
+        swap(num1, num2);
+        a.positive = -1;
+    }
+    else {
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            if (num1[i] > num2[i]) {
+                break;
+            }
+            if (num1[i] < num2[i]) {
+                swap(num1, num2);
+                a.positive = -1;
+                break;
+            }
+        }
+    }
+
+    //substraction
+    int borrow = 0, num1_len = num1.length(), num2_len = num2.length();
+    for (int i = 0; i < num1_len; i++) {
+        if (i < num2_len) {
+            int t = (num1[i] - '0') - (num2[i] - '0') + borrow;
+            if (t < 0) {
+                borrow = -1;
+                t += 10;
+            }
+            else borrow = 0;
+            ans.push_back(t);
+        }
+        else {
+            int t = (num1[i] - '0') + borrow;
+            if (t < 0) {
+                borrow = -1;
+                t += 10;
+            }
+            else borrow = 0;
+            ans.push_back(t);
+        }
+    }
+
+    //clear redundant zeros ex.1000 - 999 = 0001 -> 1
+    for (int i = ans.size() - 1; i > 0; i--) {
+        if (ans[i] != 0)
+            break;
+        ans.pop_back();
+    }
+
+    string temp;
+    for (int i = 0; i < ans.size(); i++) {
+        temp.append(1, (char)(ans[i] + '0'));
+    }
+
+    a.number = temp;
+    return a;
 }
