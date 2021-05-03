0403284652552649c4b02375f6b6fb642e6d1cf2
shinku2846 <shinku2846@gmail.com>
2021-05-03 14:22:13 +0800

    Update Big tree calculator.cpp

--- Big tree calculator.cpp
+++ Big tree calculator.cpp
@@ -506,12 +506,9 @@ Decimal Big_tree_calculator::value_process(string input) {
 						break;
 					}
 				}
-				
 				while (temp_string[temp_string.length() - 1] == '0' && temp_string.length() > 1) {
 					temp_string.pop_back();
-					point_pos--;
 				}
-				
 				Integer numerator;
 				numerator.number = temp_string;
 				Integer denominator;
